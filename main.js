ae741571d16a6882031c6a74bd3cf845ba49e50f
Shafiul Azim <shafiulmizbah2@gmail.com>
2020-11-02 10:58:18 +0600

    Digital clock V-1

---
+++ main.js
@@ -0,0 +1,23 @@
+function clock() {
+    
+      var  hour = document.getElementById('hour');
+      var  min = document.getElementById('min');
+      var  sec = document.getElementById('sec');
+      var ses= document.getElementById('session');
+
+      var h= new Date().getHours();
+      var m= new Date().getMinutes();
+      var s= new Date().getSeconds();
+      
+      if(h>12){
+          h = h-12;
+          ses.innerHTML = "PM"
+      }
+    
+      hour.innerHTML=h;
+      min.innerHTML=m;
+      sec.innerHTML=s;
+
+}
+ setInterval(clock,1000);
+
